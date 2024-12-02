befc2c108b8e3a6c5990b3ff624ad4d25049498b
MUHAMMAD ROHAIL ASHRAF <rohailashraf087@gmail.com>
2024-12-02 11:35:20 +0500

    minor changes

--- Iterative_Approach.cpp
+++ Iterative_Approach.cpp
@@ -1,6 +1,6 @@
 #include <iostream> // Include the iostream library for input and output operations
 
-using namespace std; // Use the standard namespace to avoid typing 'std::' before standard functions
+using namespace std; 
 
 // Function to compute the nth Fibonacci number using an iterative approach
 int fibonacci_iterative(int n) {
